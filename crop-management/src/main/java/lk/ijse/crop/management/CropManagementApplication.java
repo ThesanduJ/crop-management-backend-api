048cc1e1907974d9960b605fce265410d0ea340f
ThesanduJ <thesandu1602500@gmail.com>
2024-11-29 19:09:14 +0530

    Initial commit

---
+++ crop-management/src/main/java/lk/ijse/crop/management/CropManagementApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.crop.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropManagementApplication.class, args);
+	}
+
+}
