1fc395184641558bfccb40acf6c5e2a6a60edf63
ThesanduJ <thesandu1602500@gmail.com>
2024-12-01 00:28:57 +0530

    add cropEntity/superEntity

---
+++ crop-management/src/main/java/lk/ijse/crop/management/entity/impl/CropEntity.java
@@ -0,0 +1,26 @@
+package lk.ijse.crop.management.entity.impl;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.ijse.crop.management.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "crop")
+public class CropEntity implements SuperEntity {
+    @Id
+    private String  cropCode;
+    private String cropCommonName;
+    private String cropScientificName;
+    @Column(columnDefinition = "LONGTEXT")
+    private String cropImage;
+    private String cropCategory;
+    private String cropSeason;
+}
