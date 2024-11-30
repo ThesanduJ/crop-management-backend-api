d219bc9c48ac63726df6d6ce301c0aa570fe110e
ThesanduJ <thesandu1602500@gmail.com>
2024-12-01 00:25:48 +0530

    add mapping class

---
+++ crop-management/src/main/java/lk/ijse/crop/management/util/Mapping.java
@@ -0,0 +1,17 @@
+package lk.ijse.crop.management.util;
+
+import lk.ijse.crop.management.dto.impl.CropDTO;
+import lk.ijse.crop.management.entity.impl.CropEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public CropEntity toCropEntity(CropDTO cropDTO) {
+        return modelMapper.map(cropDTO, CropEntity.class);
+    }
+}
