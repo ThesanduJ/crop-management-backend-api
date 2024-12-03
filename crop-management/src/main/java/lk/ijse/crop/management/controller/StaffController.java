ccf9a92ffaeab070dc1c3ac452f3a6dc8c592d45
ThesanduJ <thesandu1602500@gmail.com>
2024-12-03 22:04:24 +0530

    create controller class for staff

---
+++ crop-management/src/main/java/lk/ijse/crop/management/controller/StaffController.java
@@ -0,0 +1,10 @@
+package lk.ijse.crop.management.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/staff")
+public class StaffController {
+
+}
