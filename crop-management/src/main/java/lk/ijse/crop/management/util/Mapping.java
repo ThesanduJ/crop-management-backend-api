dd2f7bfbd9d59ff2590675527782b4e381c509e3
ThesanduJ <thesandu1602500@gmail.com>
2024-12-04 18:21:21 +0530

    add staff mapping

--- crop-management/src/main/java/lk/ijse/crop/management/util/Mapping.java
+++ crop-management/src/main/java/lk/ijse/crop/management/util/Mapping.java
@@ -1,7 +1,9 @@
 package lk.ijse.crop.management.util;
 
 import lk.ijse.crop.management.dto.impl.CropDTO;
+import lk.ijse.crop.management.dto.impl.StaffDTO;
 import lk.ijse.crop.management.entity.impl.CropEntity;
+import lk.ijse.crop.management.entity.impl.StaffEntity;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ public class Mapping {
     @Autowired
     private ModelMapper modelMapper;
 
+    //for crop mapping
     public CropEntity toCropEntity(CropDTO cropDTO) {
         return modelMapper.map(cropDTO, CropEntity.class);
     }
@@ -22,4 +25,14 @@ public class Mapping {
         return modelMapper.map(cropEntities, new TypeToken<List<CropDTO>>() {
         }.getType());
     }
+    // for staff mapping
+    public StaffEntity toStaffEntity(StaffDTO staffDTO) {
+        return modelMapper.map(staffDTO, StaffEntity.class);
+    }
+
+    public List<StaffDTO> asStaffDTOList(List<StaffEntity> staffEntities) {
+        return modelMapper.map(staffEntities, new TypeToken<List<StaffDTO>>() {
+        }.getType());
+    }
+
 }
