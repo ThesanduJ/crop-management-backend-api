c1765dc2fcbce8ec1e19ea94998d458aef4208f4
ThesanduJ <thesandu1602500@gmail.com>
2024-12-05 17:13:57 +0530

    complete the EquipmentEntity

--- crop-management/src/main/java/lk/ijse/crop/management/entity/impl/EquipmentEntity.java
+++ crop-management/src/main/java/lk/ijse/crop/management/entity/impl/EquipmentEntity.java
@@ -1,4 +1,22 @@
 package lk.ijse.crop.management.entity.impl;
 
-public class EquipmentEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.ijse.crop.management.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "equipment")
+public class EquipmentEntity implements SuperEntity {
+    @Id
+    private String equipmentID;
+    private String equipmentName;
+    private String equipmentType;
+    private String equipmentStatus;
 }
