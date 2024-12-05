9be50db998a158814a687aacb506872a5c0264e5
ThesanduJ <thesandu1602500@gmail.com>
2024-12-05 19:44:22 +0530

    complete the FieldEntity

--- crop-management/src/main/java/lk/ijse/crop/management/entity/impl/FieldEntity.java
+++ crop-management/src/main/java/lk/ijse/crop/management/entity/impl/FieldEntity.java
@@ -1,4 +1,24 @@
 package lk.ijse.crop.management.entity.impl;
 
-public class FieldEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.ijse.crop.management.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "field")
+public class FieldEntity implements SuperEntity {
+    @Id
+    private String fieldCode;
+    private String fieldName;
+    private String fieldLocation;
+    private double fieldExtentSize;
+    private String fieldImage01;
+    private String fieldImage02;
 }
