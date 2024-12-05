d92074efbb50a652280496a4ddc5d4f4fbcbc130
ThesanduJ <thesandu1602500@gmail.com>
2024-12-05 17:14:09 +0530

    complete the EquipmentDAO

--- crop-management/src/main/java/lk/ijse/crop/management/dao/EquipmentDAO.java
+++ crop-management/src/main/java/lk/ijse/crop/management/dao/EquipmentDAO.java
@@ -1,4 +1,7 @@
 package lk.ijse.crop.management.dao;
 
-public interface EquipmentDAO {
+import lk.ijse.crop.management.entity.impl.EquipmentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipmentDAO extends JpaRepository<EquipmentEntity,String> {
 }
