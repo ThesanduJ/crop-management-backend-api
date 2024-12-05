a5e9fc505e2064b01b18a5a3f19fddaa35427693
ThesanduJ <thesandu1602500@gmail.com>
2024-12-05 20:53:54 +0530

    add field save function

--- crop-management/src/main/java/lk/ijse/crop/management/service/FieldService.java
+++ crop-management/src/main/java/lk/ijse/crop/management/service/FieldService.java
@@ -1,4 +1,7 @@
 package lk.ijse.crop.management.service;
 
+import lk.ijse.crop.management.dto.impl.FieldDTO;
+
 public interface FieldService {
+    void  saveField(FieldDTO fieldDTO);
 }
--- crop-management/src/main/java/lk/ijse/crop/management/service/impl/FieldServiceImpl.java
+++ crop-management/src/main/java/lk/ijse/crop/management/service/impl/FieldServiceImpl.java
@@ -1,4 +1,27 @@
 package lk.ijse.crop.management.service.impl;
 
-public class FieldServiceImpl {
+import lk.ijse.crop.management.dao.FieldDAO;
+import lk.ijse.crop.management.dto.impl.FieldDTO;
+import lk.ijse.crop.management.entity.impl.FieldEntity;
+import lk.ijse.crop.management.exceptions.DataPersisException;
+import lk.ijse.crop.management.service.FieldService;
+import lk.ijse.crop.management.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class FieldServiceImpl implements FieldService {
+    @Autowired
+    private FieldDAO fieldDAO;
+    @Autowired
+    private Mapping mapping;
+    @Override
+    public void saveField(FieldDTO fieldDTO) {
+        FieldEntity  saveField=fieldDAO.save(mapping.toFieldEntity(fieldDTO));
+        if (saveField==null){
+            throw new DataPersisException("Field not saved");
+        }
+    }
 }
