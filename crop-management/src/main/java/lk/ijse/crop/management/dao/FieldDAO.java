cb9f022392a6fbd238b0e7f277dac7168c602646
ThesanduJ <thesandu1602500@gmail.com>
2024-12-05 19:44:12 +0530

    complete the FieldDAO

--- crop-management/src/main/java/lk/ijse/crop/management/dao/FieldDAO.java
+++ crop-management/src/main/java/lk/ijse/crop/management/dao/FieldDAO.java
@@ -1,4 +1,9 @@
 package lk.ijse.crop.management.dao;
 
-public interface FieldDAO {
+import lk.ijse.crop.management.entity.impl.FieldEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FieldDAO extends JpaRepository<FieldEntity,String> {
 }
