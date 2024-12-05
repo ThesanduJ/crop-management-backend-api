187b05424d75fe9943e9b668ad25e358b5e88260
ThesanduJ <thesandu1602500@gmail.com>
2024-12-05 11:39:56 +0530

    change the exception

--- crop-management/src/main/java/lk/ijse/crop/management/service/impl/StaffServiceImpl.java
+++ crop-management/src/main/java/lk/ijse/crop/management/service/impl/StaffServiceImpl.java
@@ -4,13 +4,13 @@ import lk.ijse.crop.management.dao.StaffDAO;
 import lk.ijse.crop.management.dto.impl.StaffDTO;
 import lk.ijse.crop.management.entity.impl.StaffEntity;
 import lk.ijse.crop.management.exceptions.DataPersisException;
+import lk.ijse.crop.management.exceptions.StaffNotFoundException;
 import lk.ijse.crop.management.service.StaffService;
 import lk.ijse.crop.management.util.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,7 +34,7 @@ public class StaffServiceImpl implements StaffService {
     public void deleteStaff(String staffID) {
         Optional<StaffEntity> staffExists = staffDAO.findById(staffID);
         if (!staffExists.isPresent()) {
-            throw new DataPersisException("Staff with ID " + staffID + " Not Found");
+            throw new StaffNotFoundException("Staff with ID " + staffID + " Not Found");
         } else {
             staffDAO.deleteById(staffID);
         }
